9ccb0c54ab070b4aab31df49ad6484628ac1b067
Constance Chapman <constancealicechapman224@gmail.com>
2018-08-08 13:55:42 +0200

    all done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    get 'reviews/new', to: 'reviews#new', as: 'new_review'
+    post 'reviews', to: 'reviews#create', as: 'reviews'
+  end
 end
